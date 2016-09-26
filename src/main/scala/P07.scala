d2afa1e3ec9472455371062470de950113a11ae9
David Simkin <david.simkin@guardian.co.uk>
2016-09-26 11:24:23 +0100

    Make cleaner

--- src/main/scala/P07.scala
+++ src/main/scala/P07.scala
@@ -7,11 +7,9 @@ object P07 {
     println(flatten(List(List(1, 1), 2, List(3, List(5, 8)))))
   }
 
-  def flatten(list : List[Any]) : List[Any] = {
-    list.flatMap(x => x match {
-      case v : Int => List(v)
-      case l : List[Any] => flatten(l)
-      case _ => List()
-    })
+  def flatten(list : List[Any]) : List[Any] = list flatMap {
+    case v: Int => List(v)
+    case l: List[Any] => flatten(l)
+    case _ => List()
   }
 }
