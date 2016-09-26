76a5df4f14d3176038f8318d7bc0b1b69ac4e893
David Simkin <david.simkin@guardian.co.uk>
2016-09-26 10:47:43 +0100

    Problem 7

---
+++ src/main/scala/P07.scala
@@ -0,0 +1,17 @@
+/**
+  * Flatten a nested list structure.
+  */
+object P07 {
+
+  def runExample() = {
+    println(flatten(List(List(1, 1), 2, List(3, List(5, 8)))))
+  }
+
+  def flatten(list : List[Any]) : List[Any] = {
+    list.flatMap(x => x match {
+      case v : Int => List(v)
+      case l : List[Any] => flatten(l)
+      case _ => List()
+    })
+  }
+}
